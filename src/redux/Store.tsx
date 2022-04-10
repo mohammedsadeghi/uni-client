7ea95150f3d2987903daec19b950edefa2ebcee7
mohammedsadeghi <sadeghimohammad4488@gmail.com>
2022-04-10 15:35:55 +0430

    redux setup react-redux redux redux-persist redux-logger redux-js-toolkit

---
+++ src/redux/Store.tsx
@@ -0,0 +1,12 @@
+//import { persistedReducer } from "./reducers";
+import logger from "redux-logger";
+import { persistStore } from "redux-persist";
+import { configureStore } from "@reduxjs/toolkit";
+
+// const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: [logger],
+// });
+
+// export const persistor = persistStore(store);
+// export default store;
